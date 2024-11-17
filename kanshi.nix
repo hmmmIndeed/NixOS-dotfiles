d6322436a466d357522d588af75684fd89b55d49
hmmmIndeed <caitheguy3@gmail.com>
2024-11-17 12:42:48 -0500

    updated kanshi since profiles needs settings now

--- kanshi.nix
+++ kanshi.nix
@@ -4,35 +4,38 @@
 		systemdTarget = "hyprland-session.target";
 
 		# eDP-1 is the laptop screen
-		# DP-2 is the docked monitor
-		profiles = {
-			# when second monitor isn't in use
-			undocked = {
-				outputs = [
-					{
-						criteria = "eDP-1";
-						status = "enable";
-						position = "0,0";
-					}
-				];
-			};
-			# when second monitor is in use
-			docked = {
-				outputs = [
-					{
-						criteria = "eDP-1";
-						status = "enable";
-					}
+		# HDMI-A-1 is the docked monitor
+    settings = [
+      {
+        profile.name = "undocked";
+        profile.outputs = [
+          {
+					  criteria = "eDP-1";
+					  status = "enable";
+					  position = "0,0";
+          }
+        ];
+      }
+      {
+        profile.name = "docked";
+        profile.outputs = [
 					{
 						criteria = "HDMI-A-1";
 						status = "enable";
 						position = "0,0";
 					}
-				];
-			};
-		};
+					{
+						criteria = "eDP-1";
+						status = "enable";
+						position = "1920,0";
+					}
+        ];
+      }
+    ];
 	};
-};
-# sources:
+}
+# depreciated sources:
 # https://discourse.nixos.org/t/starting-kanshi-via-systemd-user-swaywm/27960
 # https://haseebmajid.dev/posts/2023-07-25-nixos-kanshi-and-hyprland/
+# up to date sources:
+# https://www.reddit.com/r/NixOS/comments/1ds8g3m/help_me_make_sense_of_kanshiprofiles_option_is/
